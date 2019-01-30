174b1d516387cb78e09d551ece56644390347c7f
charly <charly-14.old@orange.fr>
2019-01-30 23:21:06 +0100

    sidenav retiree

--- front/src/app/components/head/head.component.ts
+++ front/src/app/components/head/head.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { UsersService } from '../../services/users.service'
+import { User } from 'src/app/models/user';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+const LOCAL_STORAGE_USER_KEY = 'currentItem';
 
 @Component({
   selector: 'app-head',
@@ -9,9 +14,24 @@ import { AuthService } from '../../services/auth.service';
 export class HeadComponent implements OnInit {
   appTitle = 'FIND MY TRUCK';
 
-  constructor(private authservice: AuthService) { }
+  private currentUserSubject: BehaviorSubject<User>;
+  private user;  
+
+  constructor(
+    private authservice: AuthService,
+    private usersService: UsersService,
+  ) { 
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem(LOCAL_STORAGE_USER_KEY)));
+  }
+
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  showFiller = false;
 
   ngOnInit() {
+    this.user = this.currentUserSubject.value.id;
   }
     
 }
