6443793518a3d1595004c23365ad20f3a89a1a43
charly <charly-14.old@orange.fr>
2019-02-11 13:45:24 +0100

    modif user log

--- front/src/app/components/truckownerprofile/truckownerprofile.component.ts
+++ front/src/app/components/truckownerprofile/truckownerprofile.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, Inject, Input } from '@angular/core';
+import { FormGroup, FormBuilder} from '@angular/forms';
+import { MatSlideToggle } from '@angular/material';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { UsersService } from '../../services/users.service';
+import { User } from 'src/app/models/user';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-truckownerprofile',
